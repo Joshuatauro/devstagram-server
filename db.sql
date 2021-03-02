9067a83e77c4a215f201260136fe3a2869957d21
Joshuatauro <joshuatauro45@gmail.com>
2021-03-02 19:47:15 +0400

    Fixed deployment bug

--- db.sql
+++ db.sql
@@ -2,20 +2,19 @@
     uid VARCHAR,
     username VARCHAR(25) PRIMARY KEY,
     followed_subreddits TEXT[]
+    bio VARCHAR DEFAULT 'Hey guys, welcome to my profile',
+    profile_url VARCHAR
+    portfolio VARCHAR
   );
 
--- FOR INSERTING ARRAY INTO TABLE
-INSERT INTO users VALUES (1,'Joshua',ARRAY['1','2'])
-
-
-//CREATED TABLE
 CREATE TABLE posts (
   postId BIGSERIAL PRIMARY KEY NOT NULL,
   username VARCHAR(25) NOT NULL,
   title VARCHAR(30) NOT NULL,
-  content VARCHAR NOT NULL,
+  content VARCHAR,
   subreddit VARCHAR(20) NOT NULL,
-  createdAt TIMESTAMP NOT NULL
+  createdAt TIMESTAMP NOT NULL,
+  img_url VARCHAR 
 );
 
 CREATE TABLE likes (
@@ -25,61 +24,17 @@ CREATE TABLE likes (
   FOREIGN KEY (parent_postid) REFERENCES posts(postid)
 );
 
+CREATE TABLE comments (
+  parent_postid UUID DEFAULT uuid_generate_v4,
+  username VARCHAR NOT NULL,
+  content VARCHAR NOT NULL,
+  createdat TIMESTAMP NOT NULL,
+  comment_id UUID DEFAULT uuid_generate_v4
+)
+
 CREATE TABLE subreddits (
   subreddit VARCHAR(20) PRIMARY KEY,
-  about VARCHAR NOT NULL
+  about VARCHAR NOT NULL,
+  followed_by TEXT[]
 );
 
-SELECT comments.content, comments.username,comments.createdat, comments.comment_id 
-FROM comments 
-JOIN posts ON posts.postid = comments.parent_postid 
-WHERE posts.postid = $1
-ORDER BY createdat DESC
-
-SELECT comments.content, comments.username,comments.createdat, comments.comment_id,comments.parent_postid,posts.postid, users.profile_url
-FROM comments, users, posts
-WHERE comments.parent_postid = posts.postid AND comments.username = users.username
-ORDER BY comments.createdat DESC;
-
-SELECT posts.postid, posts.title, posts.content, posts.img_url, posts.subreddit, posts.username, posts.createdat, COUNT(comments.parent_postid) AS comments_count
-FROM posts LEFT JOIN comments ON comments.parent_postid = posts.postid
-GROUP BY posts.postid
-ORDER BY posts.createdAt;
-
-SELECT posts.postid, posts.title, posts.content, posts.img_url, posts.subreddit, posts.username, posts.createdat, COUNT(comments.parent_postid) AS comments_count
-FROM posts, comments WHERE posts.username = 'joshua_45'
-GROUP BY posts.postid
-ORDER BY posts.createdAt;
-
-SELECT posts.postid, posts.title, posts.content, posts.img_url, posts.subreddit, posts.username, posts.createdat, users.profile_url, COUNT(comments.parent_postid) AS comments_count
-FROM posts LEFT JOIN comments ON comments.parent_postid = posts.postid LEFT JOIN users ON users.username = posts.username
-GROUP BY posts.postid, users.profile_url
-ORDER BY posts.createdAt DESC
-
-SELECT posts.postid, posts.title, posts.content, posts.img_url, posts.subreddit, posts.username, posts.createdat, users.profile_url, COUNT(comments.parent_postid) AS comments_count
-FROM posts LEFT JOIN comments ON comments.parent_postid = posts.postid LEFT JOIN users ON posts.username = 'joshua_45'
-GROUP BY posts.postid, users.profile_url
-ORDER BY posts.createdAt DESC
-
-SELECT posts.postid, posts.title, posts.content, posts.img_url, posts.subreddit, posts.username, posts.createdat, users.profile_url, COUNT(comments.parent_postid) AS comments_count
-FROM posts LEFT JOIN comments ON comments.parent_postid = posts.postid LEFT JOIN users ON users.username= 'joshua_45' 
-GROUP BY posts.postid, users.profile_url
-ORDER BY posts.createdAt
-
-SELECT posts.postid, users.profile_url, users.username, comments.content
-FROM posts LEFT JOIN users ON users.username = posts.username LEFT JOIN comments ON comments.parent_postid = posts.postid;
-
-
-SELECT posts.title, posts.content, posts.username, posts.subreddit, posts.createdat, users.profile_url, posts.img_url
-FROM posts LEFT JOIN users ON posts.subreddit = ANY(users.followed_subreddits)
-WHERE user.username = 'test_account'
-
-SELECT title, content,createdat,FOLLOWED_SUBREDDITS.username,FOLLOWED_SUBREDDITS.profile_url,img_url FROM (
-  SELECT posts.title, posts.content, posts.createdat, posts.username, users.profile_url, posts.img_url, posts.subreddit
-  FROM posts LEFT JOIN users ON users.username = posts.username
-) AS FOLLOWED_SUBREDDITS, users
-WHERE subreddit = ANY(users.followed_subreddits) AND users.username = 'joshua_45';
-
-SELECT posts.title, posts.content, posts.username, posts.createdat, posts.subreddit, users.profile_url, posts.img_url
-FROM posts JOIN users ON users.username = posts.username
-WHERE posts.subreddit ILIKE 'ALL';
--- db/index.js
+++ db/index.js
@@ -1,14 +1,30 @@
 const { Pool } = require("pg")
 require("dotenv").config()
-const pool = new Pool(
-  {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT
-  }
-);
-module.exports = {
-  query: (text,params) => pool.query(text, params)
+// const pool = new Pool(
+//   {
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASS,
+//     port: process.env.DB_PORT
+//   }
+// );
+// module.exports = {
+//   query: (text,params) => pool.query(text, params)
+// }
+
+const devConfig = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT
 }
+
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL
+}
+
+const pool = new Pool(process.env.NODE_ENV === 'production' ? prodConfig : devConfig)
+
+module.exports = pool
